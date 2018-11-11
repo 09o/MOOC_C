750de0af6148bdd526a0d8473d2b332caa560b22
09o <igsober@gmail.com>
2018-11-11 09:35:11 +0300

    Nov 11 The single day

---
+++ STRCMP.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h> 
+
+int mycmp(const char* s1, const char* s2)
+{
+	while ( *s1 == *s2 && *s1 != '\0' ) {
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
+}
+
+int main(int argc, char const *argv[])
+{
+	char s1[] = "Abc";
+	char s2[] = "abc";
+	printf("%d\n", mycmp(s1,s2));
+}
